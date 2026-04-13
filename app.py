61e96c578205c94ca786ced6a73a5931704cd1a3
Ashish Mehara <viratmehara232@gmail.com>
2026-04-13 13:17:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ st.set_page_config(
 def load_my_model():
     try:
         model = keras.models.load_model(
-    "final_garbage_model.keras", 
-    compile=False, 
-    safe_mode=False
-)
+            "final_garbage_model.h5",   
+            compile=False, 
+            safe_mode=False
+        )
         return model
     except Exception as e:
         st.error(f"❌ Model load failed: {e}")
